8e65c7125456cb9cd9fae474d385ad074a1c111a
kakita7215 <kakita7215@gmail.com>
2026-01-15 17:37:24 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// 静的ファイル配信
+// 静的ファイル配信 (publicディレクトリ)
 app.use(express.static(path.join(__dirname, "public")));
+
+// ルートアクセス時にpublic/index.htmlを明示的に返す
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const server = http.createServer(app);
 
